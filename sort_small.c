8c11b45bfd4d9b8fc38784fdee747d5cd3744925
Cibermatico <149595043+Cibermatico@users.noreply.github.com>
2024-12-09 18:08:41 +0100

    Add files via upload

--- sort_small.c
+++ sort_small.c
@@ -6,7 +6,7 @@
 /*   By: lsquarci <lsquarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:47:18 by lsquarci          #+#    #+#             */
-/*   Updated: 2024/12/09 17:47:21 by lsquarci         ###   ########.fr       */
+/*   Updated: 2024/12/09 18:02:34 by lsquarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	sort_three_val(t_stack **a)
 {
-	int	min;
 	int	max;
 
-	min = ft_min(*a);
 	max = ft_max(*a);
 	if (is_sorted(*a))
 		return ;
-	if ((*a)->content == min)
+	if ((*a)->content == max)
+	{
+		ra(a, 1);
+		if (!is_sorted(*a))
+			sa(*a, 1);
+	}
+	else if ((*a)->next->content == max)
 	{
 		rra(a, 1);
-		sa(*a, 1);
-		return ;
+		if (!is_sorted(*a))
+			sa(*a, 1);
 	}
-	if ((*a)->content == max)
-		ra(a, 1);
-	if ((*a)->next->content == max)
-		rra(a, 1);
-	if (!is_sorted(*a))
+	else
 		sa(*a, 1);
 }
 
